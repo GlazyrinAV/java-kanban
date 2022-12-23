50fa1063ec45f1596da8b5c23d1a39c051c88d44
AVG <glazyrinav1986@yandex.ru>
2022-12-23 12:37:13 +0300

    fix - изменены методы в TaskManager. Исключен вывод информации. Добавлен возврат значений из методов - изменен Main с учетом измененых методов feat - добавлен README.md

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,15 @@ public class Main {
         manager.newSubtask(2, "Sub 2", "Description Sub 2");
 
         System.out.println("-- Печать всех задач");
-        manager.printAllTasks(manager.getSimpleTasks(), manager.getEpics());
+        manager.getAllTasks(manager.getSimpleTasks(), manager.getEpics()).toString();
 
         System.out.println("-- Поиск задачи номер 3 и номер 9");
-        manager.getTaskById(3, manager.getSimpleTasks(), manager.getEpics());
-        manager.getTaskById(9, manager.getSimpleTasks(), manager.getEpics());
+        if (manager.getTaskById(3, manager.getSimpleTasks(), manager.getEpics()) != null)
+            System.out.println(manager.getTaskById(3, manager.getSimpleTasks(), manager.getEpics()));
+        else System.out.println("null");
+        if (manager.getTaskById(9, manager.getSimpleTasks(), manager.getEpics()) != null)
+            System.out.println(manager.getTaskById(9, manager.getSimpleTasks(), manager.getEpics()).toString());
+        else System.out.println("null");
 
         System.out.println("-- Замена простой задачи и 2-х подзадач в эпике.");
         manager.updateTask(1, "Task1-2", "Description of Task 1-2", 2);
@@ -20,24 +24,32 @@ public class Main {
         manager.updateTask(4, "Sub 2-2", "Description Sub 2-2", 2);
 
         System.out.println("-- Печать всех задач");
-        manager.printAllTasks(manager.getSimpleTasks(), manager.getEpics());
+        manager.getAllTasks(manager.getSimpleTasks(), manager.getEpics()).toString();
 
         System.out.println("-- Очистка задачи 4 из эпика");
         manager.removeTaskById(4);
 
         System.out.println("-- Печать подзадач из эпика");
-        manager.getSubTasksOfEpicById(2);
+        if (manager.getSubTasksOfEpicById(2) != null)
+            System.out.println(manager.getSubTasksOfEpicById(2).values());
+        else System.out.println("null");
 
-        System.out.println("-- Замена эпика.");
-        manager.updateTask(2, "Epic 1-2", "Description of Epic 1", 2);
+        System.out.println("-- Замена эпика с сохранением подзадач.");
+        manager.updateTask(2, "Epic 1-2", "Description of Epic 1-2", true);
 
         System.out.println("-- Печать всех задач");
-        manager.printAllTasks(manager.getSimpleTasks(), manager.getEpics());
+        manager.getAllTasks(manager.getSimpleTasks(), manager.getEpics()).toString();
+
+        System.out.println("-- Замена эпика без сохранения подзадач.");
+        manager.updateTask(2, "Epic 1-3", "Description of Epic 1-3", false);
+
+        System.out.println("-- Печать всех задач");
+        manager.getAllTasks(manager.getSimpleTasks(), manager.getEpics()).toString();
 
         System.out.println("-- Очистка всех задач");
         manager.clearAllTasks();
 
         System.out.println("-- Печать всех задач");
-        manager.printAllTasks(manager.getSimpleTasks(), manager.getEpics());
+        manager.getAllTasks(manager.getSimpleTasks(), manager.getEpics()).toString();
     }
 }
--- src/Subtask.java
+++ src/Subtask.java
@@ -1,9 +1,25 @@
 public class Subtask extends Task {
+    /**
+     * Конструктор для создания новых подзадач
+     * Присваивается новый порядковый номер
+     * Статус для новых задач NEW
+     * @param taskTitle - название подзадачи
+     * @param taskDescription - описание подзадачи
+     */
     public Subtask(String taskTitle, String taskDescription) {
         super(taskTitle, taskDescription);
         setTaskStatus(0);
     }
 
+    /**
+     * Конструктор для обновления подзадач
+     * Номер остается прежним и указывается при обновлении
+     * Статус указывается при обновлении
+     * @param taskTitle
+     * @param taskDescription
+     * @param taskIdNumber
+     * @param taskStatus
+     */
     public Subtask(String taskTitle, String taskDescription, int taskIdNumber, int taskStatus) {
         super(taskTitle, taskDescription, taskIdNumber, taskStatus);
     }
