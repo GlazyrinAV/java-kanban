e7249db2eb618d277d1040aecda0ed602027e5e9
AVG <alex.glazyrin86@mail.ru>
2023-02-26 15:33:08 +0300

    feat - добавлены тесты и тестовый класс

--- src/Tests/FileBackedTasksManagerTest.java
+++ src/Tests/FileBackedTasksManagerTest.java
@@ -1,22 +1,32 @@
 package Tests;
 
+import Manager.FileBackedTasksManager;
 import Manager.InMemoryHistoryManager;
-import Manager.InMemoryTaskManager;
 import Manager.TaskManager;
 import Model.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileBackedTasksManagerTest extends TaskManagerTest<TaskManager>{
     @BeforeEach
+
     public void createTaskManager() {
-        setManager(new InMemoryTaskManager(new InMemoryHistoryManager()));
+        setManager(new FileBackedTasksManager(new InMemoryHistoryManager()));
     }
 
     @AfterEach
-    public void resetIdCounter() {
+    public void resetIdCounter() throws IOException {
+        deleteDataForTest();
         Task.resetCounterForTest();
     }
 
+    private void deleteDataForTest() throws IOException {
+        Path dataFile = Path.of("./Resources/Data.csv");
+        Files.delete(dataFile);
+    }
 
 }
