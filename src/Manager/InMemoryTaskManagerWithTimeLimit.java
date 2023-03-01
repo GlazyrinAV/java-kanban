a16a36b0d0d26c8bdf2a5a27d740f134281bd816
AVG <alex.glazyrin86@mail.ru>
2023-03-01 21:26:57 +0300

    feat - добавлены тесты и тестовый класс

--- src/Manager/InMemoryTaskManagerWithTimeLimit.java
+++ src/Manager/InMemoryTaskManagerWithTimeLimit.java
@@ -8,14 +8,15 @@ import java.util.HashMap;
 
 public class InMemoryTaskManagerWithTimeLimit extends InMemoryTaskManager {
 
-    HashMap<LocalDateTime, Task> freeTime;
-    HashMap<LocalDateTime, Task> busyTime;
+    HashMap<LocalDateTime, TimeLineNodes<LocalDateTime>> freeTime;
+    HashMap<LocalDateTime, TimeLineNodes<LocalDateTime>> busyTime;
+
+    int timeLimit = 5;
     private TimeLineNodes<LocalDateTime> head;
     private TimeLineNodes<LocalDateTime> tail;
 
     /**
      * Конструктор менеджера задач, в который необходимо передавать объект менеджер историй просмотра
-     *
      * @param history - объект класса менеджер историй просмотра
      */
     public InMemoryTaskManagerWithTimeLimit(InMemoryHistoryManager history) {
@@ -26,12 +27,32 @@ public class InMemoryTaskManagerWithTimeLimit extends InMemoryTaskManager {
         tail = null;
     }
 
+    public InMemoryTaskManagerWithTimeLimit(InMemoryHistoryManager history, int timeLimit) {
+        super(history);
+        freeTime = new HashMap<>();
+        busyTime = new HashMap<>();
+        head = null;
+        tail = null;
+        this.timeLimit = timeLimit;
+    }
     @Override
     public void addTaskToPrioritizedTasks(Task task) {
+        final TimeLineNodes<LocalDateTime> oldHead = head;
+        final TimeLineNodes<LocalDateTime> newNode = new TimeLineNodes<>(task, null, oldHead, null, null);
+        head = newNode;
+        if (oldHead == null) {
+            tail = newNode;
+        } else {
+            oldHead.setNextNode(newNode);
+        }
+//
+    }
+
+    private void addTaskToBeginning(LocalDateTime time) {
 
     }
 
-    private void addTaskToBeginning(Task task) {
+    private void addTaskToEnd(Task task) {
         final TimeLineNodes<LocalDateTime> oldTail = tail;
         final TimeLineNodes<LocalDateTime> newNode = new TimeLineNodes<>(task, null, oldTail, null, null);
         tail = newNode;
@@ -40,14 +61,29 @@ public class InMemoryTaskManagerWithTimeLimit extends InMemoryTaskManager {
         } else {
             oldTail.setNextNode(newNode);
         }
-
-    }
-
-    private void addTaskToEnd(Task task) {
-
+//
     }
 
     private void addTaskToMiddle(Task task) {
 
+    }
+
+    private void removeLinksToNode(LocalDateTime time) {
+        TimeLineNodes<LocalDateTime> node = freeTime.get(time);
+        if (node.equals(head)) {
+            if (node.getNextNode() == null) {
+                head = null;
+                tail = null;
+            } else {
+                node.getNextNode().setPrevNode(null);
+                head = node.getNextNode();
+            }
+        } else if (node.equals(tail)) {
+            node.getPrevNode().setNextNode(null);
+            tail = node.getPrevNode();
+        } else {
+            node.getNextNode().setPrevNode(node.getPrevNode());
+            node.getPrevNode().setNextNode(node.getNextNode());
+        }
     }
 }
