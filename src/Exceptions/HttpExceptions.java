5e6421b32d4770f7b8d86940be966ba80e722d50
AVG <alex.glazyrin86@mail.ru>
2023-03-14 23:19:25 +0300

    feat - первая рабочая версия

--- src/Exceptions/HttpExceptions.java
+++ src/Exceptions/HttpExceptions.java
@@ -8,20 +8,8 @@ public class HttpExceptions {
         }
     }
 
-    public static class ErrorLoadingTaskServer extends RuntimeException {
-        public ErrorLoadingTaskServer(final String message) {
-            super(message);
-        }
-    }
-
     public static class ErrorInKVTaskClient extends RuntimeException {
         public ErrorInKVTaskClient(final String message) {
-            super(message);
-        }
-    }
-
-    public static class ErrorInHttpTaskManager extends RuntimeException {
-        public ErrorInHttpTaskManager(final String message) {
             super(message);
         }
     }
