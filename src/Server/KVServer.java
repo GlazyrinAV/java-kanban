7f9943202aa904179efd8422712a3c0ed5dd06ed
AVG <alex.glazyrin86@mail.ru>
2023-03-14 23:52:50 +0300

    feat - первая рабочая версия

--- src/Server/KVServer.java
+++ src/Server/KVServer.java
@@ -116,6 +116,11 @@ public class KVServer {
 		server.start();
 	}
 
+	public void stop() {
+		System.out.println("Сервер остановлен.");
+		server.stop(1);
+	}
+
 	private String generateApiToken() {
 		return "" + System.currentTimeMillis();
 	}
---
+++ src/Tests/HttpTaskManagerTest.java
@@ -0,0 +1,222 @@
+package Tests;
+
+import Manager.HttpTaskManager;
+import Manager.InMemoryHistoryManager;
+import Manager.TaskManager;
+import Model.Task;
+import Model.TaskStatus;
+import Server.KVServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+
+public class HttpTaskManagerTest extends TaskManagerTest<TaskManager> {
+
+    KVServer kvServer;
+
+    @BeforeEach
+    public void createTaskManager() throws IOException {
+        resetIdCounter();
+        kvServer = new KVServer();
+        kvServer.start();
+        String path = "http://localhost";
+        setManager(new HttpTaskManager(new InMemoryHistoryManager(), path));
+    }
+
+    @AfterEach
+    public void closeKVServer() {
+        kvServer.stop();
+    }
+//
+//    @DisplayName("Запись обычная с историей")
+//    @Test
+//    public void dataWriteBase() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test1.csv");
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        getTestManager().getTaskById(1);
+//        Assertions.assertTrue(isTwoFilesAreEqual(dataFile, testFile),
+//                "Ошибка при запаси в файл задачи и истории.");
+//    }
+//
+//    @DisplayName("Запись при пустом списке")
+//    @Test
+//    public void dataWriteWithNoTasks() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test2.csv");
+//        Assertions.assertTrue(isTwoFilesAreEqual(dataFile, testFile),
+//                "Ошибка записи в файл при отсутствии данных.");
+//    }
+//
+//    @DisplayName("Запись эпика без подзадач")
+//    @Test
+//    public void dataWriteWithEpicWithNoSubTasks() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test3.csv");
+//        getTestManager().newEpic(new NewTask("1", "1"));
+//        Assertions.assertTrue(isTwoFilesAreEqual(dataFile, testFile),
+//                "Ошибка записи данных в файл для эпика без подзадач.");
+//    }
+//
+//    @DisplayName("Запись без истории")
+//    @Test
+//    public void dataWriteWithNoHistory() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test4.csv");
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        Assertions.assertTrue(isTwoFilesAreEqual(dataFile, testFile),
+//                "Ошибка записи данных в файл без истории");
+//    }
+//
+//    @DisplayName("Чтение пустого файла")
+//    @Test
+//    public void dataReadFromFileWithNoData() {
+//        boolean isTasksEmpty = getTestManager().getAllTasks().isEmpty();
+//        boolean isHistoryEmpty = getTestManager().getHistory().isEmpty();
+//        Assertions.assertTrue(isTasksEmpty && isHistoryEmpty,
+//                "Ошибка чтении данных из пустого файла.");
+//    }
+//
+//    @DisplayName("Чтение файла с данными и историей")
+//    @Test
+//    public void dataReadFromFileWithDataAndHistory() {
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        getTestManager().newSimpleTask(new NewTask("2", "2"));
+//        getTestManager().updateTask(1, TaskStatus.IN_PROGRESS);
+//        getTestManager().getTaskById(1);
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        boolean isHistoryIsPresent = getTestManager().getHistory().equals(new ArrayList<>(List.of(1)));
+//        boolean isTaskIsPresent =
+//                checkTask(getTestManager().getTaskById(1), "1", "1", 1,
+//                        TaskStatus.IN_PROGRESS, null, 0) &&
+//                        checkTask(getTestManager().getTaskById(2), "2", "2", 2,
+//                                TaskStatus.NEW, null, 0);
+//        Assertions.assertTrue(isHistoryIsPresent && isTaskIsPresent,
+//                "Ошибка чтении данных из файла с задачами и историей.");
+//    }
+//
+//    @DisplayName("Чтение данных без истории")
+//    @Test
+//    public void dataReadFromFileWithDataAndNoHistory() {
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        getTestManager().newSimpleTask(new NewTask("2", "2"));
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        boolean isHistoryIsPresent = getTestManager().getHistory().equals(new ArrayList<>());
+//        boolean isTaskIsPresent =
+//                checkTask(getTestManager().getTaskById(1), "1", "1", 1,
+//                        TaskStatus.NEW, null, 0) &&
+//                        checkTask(getTestManager().getTaskById(2), "2", "2", 2,
+//                                TaskStatus.NEW, null, 0);
+//        Assertions.assertTrue(isHistoryIsPresent && isTaskIsPresent,
+//                "Ошибка чтении данных из файла с данными без истории.");
+//    }
+//
+//    @DisplayName("Чтение файла с эпиком без подзадач")
+//    @Test
+//    public void dataReadFromFileWithEpicWithNoSubTasks() {
+//        getTestManager().newEpic(new NewTask("1", "1"));
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        boolean isHistoryIsPresent = getTestManager().getHistory().equals(new ArrayList<>());
+//        boolean isTaskIsPresent =
+//                checkTask(getTestManager().getTaskById(1), "1", "1", 1,
+//                        TaskStatus.NEW, null, 0);
+//        Assertions.assertTrue(isHistoryIsPresent && isTaskIsPresent,
+//                "Ошибка чтении данных из файла об эпике без подзадач.");
+//    }
+//
+//    @DisplayName("Чтение файла с эпиком без подзадач")
+//    @Test
+//    public void newTaskIdAfterReadingDataFromFile() {
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        getTestManager().newSimpleTask(new NewTask("2", "2"));
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        getTestManager().newSimpleTask(new NewTask("Test", "Test"));
+//        boolean isTaskIdCorrect = getTestManager().getTaskById(3).getTaskTitle().equals("Test");
+//        Assertions.assertTrue(isTaskIdCorrect, "Ошибка в нумерации новых задач после чтения данных.");
+//    }
+//
+//    @DisplayName("Время выполнения задач Запись в файл задач со временем")
+//    @Test
+//    public void dataWriteWithTimeData() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test5.csv");
+//        getTestManager().newSimpleTask(new NewTask("1", "1",
+//                LocalDateTime.of(2022, Month.APRIL, 12, 8, 12), 30));
+//        getTestManager().newSimpleTask(new NewTask("1", "1",
+//                LocalDateTime.of(2023, Month.FEBRUARY, 28, 8, 22), 50));
+//        Assertions.assertTrue(isTwoFilesAreEqual(testFile, dataFile),
+//                "Ошибка при записи файл с данными о времени.");
+//    }
+//
+//    @DisplayName("Время выполнения задач Запись в файл задач без времени")
+//    @Test
+//    public void dataWriteWithNoTimeData() throws IOException {
+//        testFile = Path.of("./ResourcesForTest/Test6.csv");
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        getTestManager().newSimpleTask(new NewTask("2", "2",
+//                LocalDateTime.of(2023, Month.FEBRUARY, 28, 8, 22), 50));
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        Assertions.assertTrue(isTwoFilesAreEqual(testFile, dataFile),
+//                "Ошибка при записи файл с данными без времени.");
+//    }
+//
+//    @DisplayName("Время выполнения задач Чтение из файла задач со временем")
+//    @Test
+//    public void dataReadWithTimeData() {
+//        getTestManager().newSimpleTask(new NewTask("1", "1",
+//                LocalDateTime.of(2022, Month.APRIL, 12, 8, 12), 30));
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        Task task = getTestManager().getTaskById(1);
+//        Assertions.assertTrue(checkTask(task, "1", "1", 1,
+//                        TaskStatus.NEW, LocalDateTime.of(2022, Month.APRIL, 12, 8, 12), 30),
+//                "Ошибка при чтении данных из файла о задаче содержащей время выполнения.");
+//    }
+//
+//    @DisplayName("Время выполнения задач Чтение из файла задач без времени")
+//    @Test
+//    public void dataReadWithNoTimeData() {
+//        getTestManager().newSimpleTask(new NewTask("1", "1"));
+//        setManager(new FileBackedTasksManager(new InMemoryHistoryManager(), path));
+//        Task task = getTestManager().getTaskById(1);
+//        Assertions.assertTrue(checkTask(task, "1", "1", 1, TaskStatus.NEW, null, 0),
+//                "Ошибка при чтении данных из файла о задаче без данных о времени выполнения.");
+//    }
+
+    private void resetIdCounter() {
+        Task.resetCounterForTest();
+    }
+
+    private void deleteDataForTest() throws IOException {
+        Path dataFile = Path.of("./Resources/Data.csv");
+        if (Files.exists(dataFile)) {
+            Files.delete(dataFile);
+        }
+    }
+
+    private boolean isTwoFilesAreEqual(Path path1, Path path2) throws IOException {
+        final File file1 = path1.toFile();
+        final File file2 = path2.toFile();
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(file1));
+             BufferedReader reader2 = new BufferedReader(new FileReader(file2))) {
+            while (reader1.ready() && reader2.ready()) {
+                String line1 = reader1.readLine();
+                String line2 = reader2.readLine();
+                if (!line1.equals(line2)) return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkTask(Task task, String title, String description, int id, TaskStatus status,
+                              LocalDateTime starTime, int duration) {
+        return (task.getTaskTitle().equals(title)) &&
+                (task.getTaskDescription().equals(description)) &&
+                (task.getTaskIdNumber() == id) &&
+                (task.getTaskStatus().equals(status)) &&
+                ((task.getStartTime() == null && starTime == null) || task.getStartTime().equals(starTime)) &&
+                (task.getDuration() == duration);
+
+    }
+}
